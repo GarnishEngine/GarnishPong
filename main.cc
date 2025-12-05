556bc7d30432665bde6fc15976f4f2cc45216fbc
Benjamin Bingham <ben@benbingham.ca>
2025-12-05 11:27:35 -0700

    Update GarnishEngine

--- main.cc
+++ main.cc
@@ -3,5 +3,9 @@
 #include <garnish_app.hpp>
 
 int main() {
+    garnish::App::CreateInfo createInfo{ };
+    createInfo.assetPath = "3rdParty/GarnishEngine/";
+    garnish::App app{ createInfo };
+
     std::cout << "Pong" << std::endl;
 }
