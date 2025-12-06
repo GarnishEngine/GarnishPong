a3cc1f20771173d608424775217afdb3dc7390d1
Benjamin Bingham <ben@benbingham.ca>
2025-12-06 16:05:44 -0700

    Now using new engine geometry features

--- main.cc
+++ main.cc
@@ -3,6 +3,8 @@
 #include <shared.hpp>
 #include <Utility/camera.hpp>
 
+#include <geometry.hpp>
+
 int main() {
     std::string assetPath = "../3rdParty/GarnishEngine/";
     garnish::App::CreateInfo createInfo{ };
@@ -10,8 +12,9 @@ int main() {
     garnish::App app{ createInfo };
 
     // TODO asset path should be automatically added here (maybe)
-    uint32_t meshHandle = app.get_render_device()->setup_mesh(assetPath + "Models/viking_room.obj");
+    uint32_t meshHandle = app.get_render_device()->setup_mesh(createUnitCubeGeometry());
 
+    // TODO asset path should be automatically added here (maybe)
     uint32_t texHandle = app.get_render_device()->load_texture(assetPath + "Textures/viking_room.png");
 
     auto vikingRoom = app.get_controller().create_entity_with_components(
