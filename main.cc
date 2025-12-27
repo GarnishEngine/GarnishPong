01edf1954a9f53248cc668f67f2ab856b6bb89b3
Kevin Huai <142623053+KevinH15291@users.noreply.github.com>
2025-12-27 15:16:34 -0700

    Feat: Updated pong to work with ECS system changes

--- main.cc
+++ main.cc
@@ -1,23 +1,26 @@
-#include <iostream>
-#include <memory>
-
-#include <garnish_app.hpp>
-#include <shared.hpp>
-#include <Utility/camera.hpp>
-#include <geometry.hpp>
-#include <Physics/physics_system.hpp>
 #include <SDL3/SDL_keyboard.h>
 
-struct LeftPaddle{ };
-struct RightPaddle{ };
+#include <Physics/physics_system.hpp>
+#include <Utility/camera.hpp>
+#include <garnish_app.hpp>
+#include <geometry.hpp>
+#include <iostream>
+#include <memory>
+#include <shared.hpp>
+
+
+struct LeftPaddle {};
+struct RightPaddle {};
 
 float movementAcceleration = 10.0f;
 
-class LeftPaddleMovement : public garnish::System {
-    void update(garnish::ECSController& world) override {
+class LeftPaddleMovement {
+    public:
+    static void update(garnish::ECSController& world) {
         auto leftPaddles = world.get_entities<LeftPaddle, garnish::RigidBody>();
 
-        if (leftPaddles.size() > 1) std::cout << "ERROR, more then one left paddle" << std::endl;
+        if (leftPaddles.size() > 1)
+            std::cout << "ERROR, more then one left paddle" << std::endl;
 
         auto leftPaddle = leftPaddles[0];
 
@@ -27,8 +30,7 @@ class LeftPaddleMovement : public garnish::System {
         auto& rb = world.get_component<garnish::RigidBody>(leftPaddle);
         if (keyboardState[SDL_SCANCODE_W]) {
             rb.acceleration.y += movementAcceleration;
-        }
-        else if (keyboardState[SDL_SCANCODE_S]) {
+        } else if (keyboardState[SDL_SCANCODE_S]) {
             rb.acceleration.y -= movementAcceleration;
         }
 
@@ -36,11 +38,14 @@ class LeftPaddleMovement : public garnish::System {
     }
 };
 
-class RightPaddleMovement : public garnish::System {
-    void update(garnish::ECSController& world) override {
-        auto rightPaddles = world.get_entities<RightPaddle, garnish::RigidBody>();
+class RightPaddleMovement {
+    public:
+    static void update(garnish::ECSController& world) {
+        auto rightPaddles =
+            world.get_entities<RightPaddle, garnish::RigidBody>();
 
-        if (rightPaddles.size() > 1) std::cout << "ERROR, more then one right paddle" << std::endl;
+        if (rightPaddles.size() > 1)
+            std::cout << "ERROR, more then one right paddle" << std::endl;
 
         auto rightPaddle = rightPaddles[0];
 
@@ -50,8 +55,7 @@ class RightPaddleMovement : public garnish::System {
         auto& rb = world.get_component<garnish::RigidBody>(rightPaddle);
         if (keyboardState[SDL_SCANCODE_UP]) {
             rb.acceleration.y += movementAcceleration;
-        }
-        else if (keyboardState[SDL_SCANCODE_DOWN]) {
+        } else if (keyboardState[SDL_SCANCODE_DOWN]) {
             rb.acceleration.y -= movementAcceleration;
         }
 
@@ -61,43 +65,57 @@ class RightPaddleMovement : public garnish::System {
 
 int main() {
     std::string engineAssetPath = "../3rdParty/GarnishEngine/";
-    garnish::App::CreateInfo createInfo{ };
+    garnish::App::CreateInfo createInfo{.backend = garnish::RenderingBackend::OpenGL};
     createInfo.assetPath = engineAssetPath;
-    garnish::App app{ createInfo };
+    garnish::App app{createInfo};
 
     app.get_controller().register_component<LeftPaddle>();
     app.get_controller().register_component<RightPaddle>();
 
-    app.get_controller().register_system<LeftPaddleMovement>(0);
-    app.get_controller().register_system<RightPaddleMovement>(0);
+    LeftPaddleMovement leftPaddleMovement;
+    RightPaddleMovement rightPaddleMovement;
 
-    uint32_t unitCube = app.get_render_device()->setup_mesh(createUnitCubeGeometry());
+    app.register_update_function(LeftPaddleMovement::update);
+    app.register_update_function(RightPaddleMovement::update);
+
+    uint32_t unitCube =
+        app.get_render_device()->setup_mesh(createUnitCubeGeometry());
 
     // TODO need to sort out these paths
-    uint32_t whiteTexture = app.get_render_device()->load_texture(engineAssetPath + "../../assets/white-square.png");
+    uint32_t whiteTexture = app.get_render_device()->load_texture(
+        engineAssetPath + "../../assets/white-square.png");
 
     auto ball = app.get_controller().create_entity_with_components(
         garnish::Renderable{.meshHandle = unitCube, .texHandle = whiteTexture},
-        garnish::Transform{ .position = glm::vec3{ 0.0f, 0.0f, 0.0f } },
-        garnish::RigidBody{ .velocity = glm::vec3{ 2.0f, 0.0f, 0.0f }, .acceleration = glm::vec3{ 0.0f }, .inv_mass = 100.0f, .dampening = 1.0f },
-        garnish::SphereCollider{ .radius = 0.5f, .restitutionCoefficient = 1.0f }
-    );
+        garnish::Transform{.position = glm::vec3{0.0f, 0.0f, 0.0f}},
+        garnish::RigidBody{.velocity = glm::vec3{2.0f, 0.0f, 0.0f},
+                           .acceleration = glm::vec3{0.0f},
+                           .inv_mass = 100.0f,
+                           .dampening = 1.0f},
+        garnish::SphereCollider{.radius = 0.5f,
+                                .restitutionCoefficient = 1.0f});
 
     auto leftPaddle = app.get_controller().create_entity_with_components(
-        LeftPaddle{ },
+        LeftPaddle{},
         garnish::Renderable{.meshHandle = unitCube, .texHandle = whiteTexture},
-        garnish::Transform{ .position = glm::vec3{ -8.0f, 0.0f, 0.0f } },
-        garnish::RigidBody{ .velocity = glm::vec3{ 0.0f, 0.0f, 0.0f }, .acceleration = glm::vec3{ 0.0f }, .inv_mass = 0.000001f, .dampening = 0.1f },
-        garnish::SphereCollider{ .radius = 0.5f, .restitutionCoefficient = 1.0f }
-    );
+        garnish::Transform{.position = glm::vec3{-8.0f, 0.0f, 0.0f}},
+        garnish::RigidBody{.velocity = glm::vec3{0.0f, 0.0f, 0.0f},
+                           .acceleration = glm::vec3{0.0f},
+                           .inv_mass = 0.000001f,
+                           .dampening = 0.1f},
+        garnish::SphereCollider{.radius = 0.5f,
+                                .restitutionCoefficient = 1.0f});
 
     auto rightPaddle = app.get_controller().create_entity_with_components(
-        RightPaddle{ },
+        RightPaddle{},
         garnish::Renderable{.meshHandle = unitCube, .texHandle = whiteTexture},
-        garnish::Transform{ .position = glm::vec3{ 8.0f, 0.0f, 0.0f } },
-        garnish::RigidBody{ .velocity = glm::vec3{ 0.0f, 0.0f, 0.0f }, .acceleration = glm::vec3{ 0.0f }, .inv_mass = 0.000001f, .dampening = 0.1f },
-        garnish::SphereCollider{ .radius = 0.5f, .restitutionCoefficient = 1.0f }
-    );
+        garnish::Transform{.position = glm::vec3{8.0f, 0.0f, 0.0f}},
+        garnish::RigidBody{.velocity = glm::vec3{0.0f, 0.0f, 0.0f},
+                           .acceleration = glm::vec3{0.0f},
+                           .inv_mass = 0.000001f,
+                           .dampening = 0.1f},
+        garnish::SphereCollider{.radius = 0.5f,
+                                .restitutionCoefficient = 1.0f});
 
     int leftSide = -11;
     int rightSide = 11;
@@ -106,37 +124,57 @@ int main() {
 
     for (int x = leftSide; x < rightSide; ++x) {
         app.get_controller().create_entity_with_components(
-            garnish::Renderable{.meshHandle = unitCube, .texHandle = whiteTexture},
-            garnish::Transform{ .position = glm::vec3{ (float)x, (float)top, 0.0f } },
-            garnish::RigidBody{ .velocity = glm::vec3{ 0.0f, 0.0f, 0.0f }, .acceleration = glm::vec3{ 0.0f }, .inv_mass = 0.00000001f, .dampening = 0.1f },
-            garnish::SphereCollider{ .radius = 0.4f, .restitutionCoefficient = 1.0f }
-        );
+            garnish::Renderable{.meshHandle = unitCube,
+                                .texHandle = whiteTexture},
+            garnish::Transform{.position =
+                                   glm::vec3{(float)x, (float)top, 0.0f}},
+            garnish::RigidBody{.velocity = glm::vec3{0.0f, 0.0f, 0.0f},
+                               .acceleration = glm::vec3{0.0f},
+                               .inv_mass = 0.00000001f,
+                               .dampening = 0.1f},
+            garnish::SphereCollider{.radius = 0.4f,
+                                    .restitutionCoefficient = 1.0f});
 
         app.get_controller().create_entity_with_components(
-            garnish::Renderable{.meshHandle = unitCube, .texHandle = whiteTexture},
-            garnish::Transform{ .position = glm::vec3{ (float)x, (float)bottom, 0.0f } },
-            garnish::RigidBody{ .velocity = glm::vec3{ 0.0f, 0.0f, 0.0f }, .acceleration = glm::vec3{ 0.0f }, .inv_mass = 0.00000001f, .dampening = 0.1f },
-            garnish::SphereCollider{ .radius = 0.4f, .restitutionCoefficient = 1.0f }
-        );
+            garnish::Renderable{.meshHandle = unitCube,
+                                .texHandle = whiteTexture},
+            garnish::Transform{.position =
+                                   glm::vec3{(float)x, (float)bottom, 0.0f}},
+            garnish::RigidBody{.velocity = glm::vec3{0.0f, 0.0f, 0.0f},
+                               .acceleration = glm::vec3{0.0f},
+                               .inv_mass = 0.00000001f,
+                               .dampening = 0.1f},
+            garnish::SphereCollider{.radius = 0.4f,
+                                    .restitutionCoefficient = 1.0f});
     }
 
     for (int y = bottom; y < top; ++y) {
         app.get_controller().create_entity_with_components(
-            garnish::Renderable{.meshHandle = unitCube, .texHandle = whiteTexture},
-            garnish::Transform{ .position = glm::vec3{ (float)leftSide - 0.5f, (float)y, 0.0f } },
-            garnish::RigidBody{ .velocity = glm::vec3{ 0.0f, 0.0f, 0.0f }, .acceleration = glm::vec3{ 0.0f }, .inv_mass = 0.00000001f, .dampening = 0.1f },
-            garnish::SphereCollider{ .radius = 0.4f, .restitutionCoefficient = 1.0f }
-        );
+            garnish::Renderable{.meshHandle = unitCube,
+                                .texHandle = whiteTexture},
+            garnish::Transform{
+                .position = glm::vec3{(float)leftSide - 0.5f, (float)y, 0.0f}},
+            garnish::RigidBody{.velocity = glm::vec3{0.0f, 0.0f, 0.0f},
+                               .acceleration = glm::vec3{0.0f},
+                               .inv_mass = 0.00000001f,
+                               .dampening = 0.1f},
+            garnish::SphereCollider{.radius = 0.4f,
+                                    .restitutionCoefficient = 1.0f});
 
         app.get_controller().create_entity_with_components(
-            garnish::Renderable{.meshHandle = unitCube, .texHandle = whiteTexture},
-            garnish::Transform{ .position = glm::vec3{ (float)rightSide + 0.5f, (float)y, 0.0f } },
-            garnish::RigidBody{ .velocity = glm::vec3{ 0.0f, 0.0f, 0.0f }, .acceleration = glm::vec3{ 0.0f }, .inv_mass = 0.00000001f, .dampening = 0.1f },
-            garnish::SphereCollider{ .radius = 0.4f, .restitutionCoefficient = 1.0f }
-        );
+            garnish::Renderable{.meshHandle = unitCube,
+                                .texHandle = whiteTexture},
+            garnish::Transform{
+                .position = glm::vec3{(float)rightSide + 0.5f, (float)y, 0.0f}},
+            garnish::RigidBody{.velocity = glm::vec3{0.0f, 0.0f, 0.0f},
+                               .acceleration = glm::vec3{0.0f},
+                               .inv_mass = 0.00000001f,
+                               .dampening = 0.1f},
+            garnish::SphereCollider{.radius = 0.4f,
+                                    .restitutionCoefficient = 1.0f});
     }
 
-    garnish::Camera cam{ };
+    garnish::Camera cam{};
     cam.position.z = 15.0f;
     app.get_controller().create_entity_with_components(cam);
 
